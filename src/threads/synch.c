9833263a4c47e07805aa8d2a1a667fcf59fe9e14
Yuchen Sun <32922504+lyudmilalala@users.noreply.github.com>
2018-05-04 13:40:33 -0400

    Update synch.c

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -280,7 +280,7 @@ lock_release (struct lock *lock)
   ASSERT (lock != NULL);
   ASSERT (lock_held_by_current_thread (lock));
 
- // lock->holder = NULL;
+  lock->holder = NULL;
   sema_up (&lock->semaphore);
 }
 
